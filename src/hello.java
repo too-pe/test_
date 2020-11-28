8d342b8a7ce0c0d6cf18b6f10222e8d535a9c63b
too-pe <2019282008@email.szu.edu.cn>
2020-11-28 19:07:14 +0800

    test

---
+++ src/hello.java
@@ -0,0 +1,7 @@
+public class hello {
+    public static void main(String[] args){
+        System.out.println("hello world!");
+        System.out.println("good morning!");
+        System.out.println("so hello?");
+    }
+}
